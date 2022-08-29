aea3eb25a2acbaf94242379a568fedeb77529e66
Erick Michel <michel172002@gmail.com>
2022-08-28 23:11:52 -0300

    Adicionando arquivos do projeto

---
+++ Main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import selenium.common.exceptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+# links e xpaths
+WPP_LINK = "https://web.whatsapp.com/"
+SEND_BUTTON = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
+START_CHAT = '//*[@id="action-button"]'
+USE_WEB_WPP = '//*[@id="fallback_block"]/div/div/h4[2]/a'
+INV_BUTTON = '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[2]/div/div'
+
+driver = webdriver.Chrome()
+driver.get(WPP_LINK)  # abre o whatsapp. necessita autenticacao via QRCode
+sleep(20)
+
+#Verifica se já foi conectado o whatsapp
+try:
+    while driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[1]/div/a'):
+        sleep(1)
+except selenium.common.exceptions.NoSuchElementException:
+    print("Usuario Conectado!")
+
+def send_message(numero, msg):
+    # chama a api para enviar mensagem a um numero
+    url = "https://web.whatsapp.com/send?phone={}&text={}"
+    driver.get(url.format(numero, msg))
+    sleep(20)
+
+    #Verifica se o contato existe
+    try:
+        # clica no botao de enviar mensagem
+        send_button = driver.find_element(By.XPATH, SEND_BUTTON)
+        send_button.click()
+        sleep(3)
+    except selenium.common.exceptions.NoSuchElementException:
+        #caso o numero não existe vai fechar a mensagem
+        driver.find_element(By.XPATH, INV_BUTTON).click()
+
+
+# coloque o caminho do arquivo
+df = pd.read_excel('lista_contatos_bot_wpp.xlsx')
+numeros = df['Telefone'].values.tolist()
+nomes = df['Nome'].values.tolist()
+# defina abaixo a mensagem que quer enviar
+mensagem = "Ola {}, sou um bot te enviando uma mensagem!"
+
+for numero, nome in zip(numeros, nomes):
+    send_message(numero, mensagem.format(nome))
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Bot para enviar mensagem no whatsapp, para contatos salvos em excel!
+
+links para saber os drivers de cada navegador: https://pypi.org/project/webdriver-manager/
